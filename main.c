2e421e855f4732a4fa4ef0a6102a1dc394670451
Turbobent <kasp728h@edu.mercantec.dk>
2025-07-03 13:49:55 +0200

    2 players

--- main.c
+++ main.c
@@ -17,6 +17,10 @@ typedef struct {
 Pipe pipes[MAX_PIPES];
 int birdY = 200, velocity = 0;
 int isGameOver = 0;
+int bird2Y = 200, velocity2 = 0;
+int isGameOver1 = 0;
+int isGameOver2 = 0;
+
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
     static int frame = 0;
@@ -34,17 +38,25 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
             break;
 
         case WM_KEYDOWN:
-            if (wParam == VK_SPACE)
-                velocity = -10;
+            if (wParam == VK_UP)
+            velocity = -10;
+        else if (wParam == 'W' || wParam == 'w')  
+            velocity2 = -10;
             break;
 
+
    case WM_TIMER:
-    if (isGameOver) break;
+    if (isGameOver1 && isGameOver2) break;
 
     velocity += 1;
     birdY += velocity;
     if (birdY < 0) birdY = 0;
     if (birdY > 570) birdY = 570;
+
+    velocity2 += 1;
+    bird2Y += velocity2;
+    if (bird2Y < 0) bird2Y = 0;
+    if (bird2Y > 570) bird2Y = 570;
 
     // Move pipes
     for (int i = 0; i < MAX_PIPES; i++) {
@@ -55,48 +67,66 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
         }
     }
 
-    // Collision detection
-    for (int i = 0; i < MAX_PIPES; i++) {
-        int pipeX = pipes[i].x;
-        int pipeTop = pipes[i].gapY;
-        int pipeBottom = pipeTop + PIPE_GAP;
+    // Collision detection for Player 1
+    if (!isGameOver1) {
+        for (int i = 0; i < MAX_PIPES; i++) {
+            int pipeX = pipes[i].x;
+            int pipeTop = pipes[i].gapY;
+            int pipeBottom = pipeTop + PIPE_GAP;
 
-        int birdLeft = 100;
-        int birdRight = 100 + BIRD_WIDTH;
-        int birdTop = birdY;
-        int birdBottom = birdY + BIRD_HEIGHT;
+            int birdLeft = 100;
+            int birdRight = 100 + BIRD_WIDTH;
+            int birdTop = birdY;
+            int birdBottom = birdY + BIRD_HEIGHT;
 
-        int pipeLeft = pipeX;
-        int pipeRight = pipeX + PIPE_WIDTH;
-
-        if (birdRight > pipeLeft && birdLeft < pipeRight) {
-            if (birdTop < pipeTop || birdBottom > pipeBottom) {
-                isGameOver = 1;
-                KillTimer(hwnd, 1);
-                int result = MessageBox(hwnd, "Game Over! Try again?", "Collision", MB_YESNO | MB_ICONQUESTION);
-                if (result == IDYES) {
-                    // Reset game state
-                    birdY = 200;
-                    velocity = 0;
-                    for (int j = 0; j < MAX_PIPES; j++) {
-                        pipes[j].x = 800 + j * 300;
-                        pipes[j].gapY = 100 + rand() % 300;
-                    }
-                    isGameOver = 0;
-                    SetTimer(hwnd, 1, 16, NULL);
-                } else {
-                    PostQuitMessage(0);
+            if (birdRight > pipeX && birdLeft < pipeX + PIPE_WIDTH) {
+                if (birdTop < pipeTop || birdBottom > pipeBottom) {
+                    isGameOver1 = 1;
                 }
-                break;
             }
+        }
+    }
+
+    // Collision detection for Player 2
+    if (!isGameOver2) {
+        for (int i = 0; i < MAX_PIPES; i++) {
+            int pipeX = pipes[i].x;
+            int pipeTop = pipes[i].gapY;
+            int pipeBottom = pipeTop + PIPE_GAP;
+
+            int birdLeft = 150;
+            int birdRight = 150 + BIRD_WIDTH;
+            int birdTop = bird2Y;
+            int birdBottom = bird2Y + BIRD_HEIGHT;
+
+            if (birdRight > pipeX && birdLeft < pipeX + PIPE_WIDTH) {
+                if (birdTop < pipeTop || birdBottom > pipeBottom) {
+                    isGameOver2 = 1;
+                }
+            }
+        }
+    }
+
+    // If both dead, ask to restart
+    if (isGameOver1 && isGameOver2) {
+        KillTimer(hwnd, 1);
+        int result = MessageBox(hwnd, "Both players lost! Restart?", "Game Over", MB_YESNO | MB_ICONQUESTION);
+        if (result == IDYES) {
+            birdY = bird2Y = 200;
+            velocity = velocity2 = 0;
+            for (int j = 0; j < MAX_PIPES; j++) {
+                pipes[j].x = 800 + j * 300;
+                pipes[j].gapY = 100 + rand() % 300;
+            }
+            isGameOver1 = isGameOver2 = 0;
+            SetTimer(hwnd, 1, 16, NULL);
+        } else {
+            PostQuitMessage(0);
         }
     }
 
     InvalidateRect(hwnd, NULL, FALSE);
     break;
-
-
-
         case WM_PAINT: {
             PAINTSTRUCT ps;
             HDC hdc = BeginPaint(hwnd, &ps);
@@ -106,11 +136,18 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
             FillRect(hdc, &ps.rcPaint, sky);
             DeleteObject(sky);
 
-            // Draw bird
-            HBRUSH birdBrush = CreateSolidBrush(RGB(255, 255, 0));
-            SelectObject(hdc, birdBrush);
+            // Draw bird 1 (yellow)
+            HBRUSH birdBrush1 = CreateSolidBrush(RGB(255, 255, 0));
+            SelectObject(hdc, birdBrush1);
             Rectangle(hdc, 100, birdY, 100 + BIRD_WIDTH, birdY + BIRD_HEIGHT);
-            DeleteObject(birdBrush);
+            DeleteObject(birdBrush1);
+
+            // Draw bird 2 (red)
+            HBRUSH birdBrush2 = CreateSolidBrush(RGB(255, 0, 0));
+            SelectObject(hdc, birdBrush2);
+            Rectangle(hdc, 150, bird2Y, 150 + BIRD_WIDTH, bird2Y + BIRD_HEIGHT);
+            DeleteObject(birdBrush2);
+
 
             // Draw pipes
             HBRUSH pipeBrush = CreateSolidBrush(RGB(0, 200, 0));
